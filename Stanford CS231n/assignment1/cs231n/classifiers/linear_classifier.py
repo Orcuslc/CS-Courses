cb786b8d694d19b827690a77dd72103830f13091
orcuslc <orcuslc@hotmail.com>
2017-11-15 16:14:55 -0600

    update linear classifier

--- Stanford CS231n/assignment1/cs231n/classifiers/linear_classifier.py
+++ Stanford CS231n/assignment1/cs231n/classifiers/linear_classifier.py
@@ -39,8 +39,9 @@ class LinearClassifier(object):
     # Run stochastic gradient descent to optimize W
     loss_history = []
     for it in xrange(num_iters):
-      X_batch = None
-      y_batch = None
+      choice = np.random.choice(range(num_train), batch_size)
+      X_batch = X[choice, :]
+      y_batch = y[choice]
 
       #########################################################################
       # TODO:                                                                 #
@@ -67,7 +68,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W -= learning_rate*grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -96,7 +97,8 @@ class LinearClassifier(object):
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    scores = X.dot(self.W)
+    y_pred = np.argmax(scores, axis = 1)
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
